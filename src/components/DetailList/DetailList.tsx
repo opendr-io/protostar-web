f1d3a5b1c7c77200a9adcc0a0118b9702555e8d0
kaushikSarma <kausyap10+github@github.com>
2024-07-31 21:14:50 +0530

    chore: add guid, indentation

--- src/components/DetailList/DetailList.tsx
+++ src/components/DetailList/DetailList.tsx
@@ -5,7 +5,6 @@ function AlertRow({ alertData, index }: { alertData: AlertNodeData, index: numbe
   // comment the fields names that need to be hidden in the Alert details
   // any new field name can be added here if needed 
   const detailKeysToShow: (keyof AlertNodeData)[] = [
-    // "guid",
     "name",
     "detection_type",
     "category",
@@ -19,12 +18,18 @@ function AlertRow({ alertData, index }: { alertData: AlertNodeData, index: numbe
     "process",
     "proctitle",
     "severity",
+    "guid",
   ];
 
   return (
     <div className='alertrow' data-index={`#${index}`}>
-      <div className='alertHeader'><h3>{new Date(alertData.timestamp).toUTCString()}</h3><h3>{alertData?.entity_type}</h3><h3>{alertData?.host_ip}</h3><h3>{alertData?.entity}</h3></div>
-      <div className='alertDetails'>{detailKeysToShow.map((key, index) => {
+      <div className='alertHeader'>
+        <h3>{new Date(alertData.timestamp).toUTCString()}</h3>
+        <h3>{alertData?.entity_type}</h3>
+        <h3>{alertData?.host_ip}</h3>
+        <h3>{alertData?.entity}</h3>
+      </div>
+      <div className='alertDetails'>{detailKeysToShow.map((key) => {
         if (alertData[key] && alertData[key] !== '-') {
           return <div>{key}: "{alertData[key]}",</div>
         }
