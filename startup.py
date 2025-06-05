39b53697ef2bdf294e768bf77b296275b416db9b
anidev95 <130506893+anidev95@users.noreply.github.com>
2025-06-05 11:00:07 -0400

    Code formatting

--- startup.py
+++ startup.py
@@ -1,14 +1,14 @@
 import subprocess
 import concurrent.futures
 
+def execute_scripts(command, directory):
+  try:
+    result = subprocess.run(command, cwd=directory, shell=True, capture_output=True, text=True)
+    return command, directory, result.stdout, result.stderr, result.returncode
+  except Exception as e:
+    return command, directory, "", str(e), -1
+
 def run():
-  def execute_scripts(command, directory):
-    try:
-      result = subprocess.run(command, cwd=directory, shell=True, capture_output=True, text=True)
-      return command, directory, result.stdout, result.stderr, result.returncode
-    except Exception as e:
-      return command, directory, "", str(e), -1
-    
   commands = ['npm start', 'flask --app skynet-ai-dev-flask-api run --host 0.0.0.0 --port 5002', 'npm run dev']
   directories = ['skynet-neo', 'skynet-ai-dev-flask-api', 'skynet-react']
     
