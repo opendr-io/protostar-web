162e04821960046f324202ede9c9eefb738bc0c4
anidev95 <130506893+anidev95@users.noreply.github.com>
2025-06-05 09:43:12 -0400

    Changes

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -8,33 +8,33 @@ const SidebarRow: React.FC<React.PropsWithChildren<unknown>> = ({ children }) =>
   </div>
 }
 
-export function Sidebar() {
-  return (
-    <aside className={'sidebar'}>
-      <h2 style={{ marginTop: 48, marginLeft: 12 }}>{'Dashboards'}</h2>
-      <Link to={'/view1'}>
-        <SidebarRow>{'Everything'}</SidebarRow>
-      </Link>
-      <Link to={'/view2'}>
-        <SidebarRow>{'Primary View'}</SidebarRow>
-      </Link>
-      <Link to={'/view3'}>
-        <SidebarRow>{'Entity'}</SidebarRow>
-      </Link>
+// export function Sidebar() {
+//   return (
+//     <aside className={'sidebar'}>
+//       <h2 style={{ marginTop: 48, marginLeft: 12 }}>{'Dashboards'}</h2>
+//       <Link to={'/view1'}>
+//         <SidebarRow>{'Everything'}</SidebarRow>
+//       </Link>
+//       <Link to={'/view2'}>
+//         <SidebarRow>{'Primary View'}</SidebarRow>
+//       </Link>
+//       <Link to={'/view3'}>
+//         <SidebarRow>{'Entity'}</SidebarRow>
+//       </Link>
 
-      <h2 style={{ marginTop: 48, marginLeft: 12 }}>{'Experimental'}</h2>
-      <Link to={'/experiment/view4'}>
-        <SidebarRow>{'View4'}</SidebarRow>
-      </Link>
-      <Link to={'/experiment/view5'}>
-        <SidebarRow>{'View5'}</SidebarRow>
-      </Link>
-      <Link to={'/experiment/view6'}>
-        <SidebarRow>{'View6 - Cloud Detections'}</SidebarRow>
-      </Link>
-      <Link to={'/experiment/view7'}>
-        <SidebarRow>{'View7 - Terrestrial Detections'}</SidebarRow>
-      </Link>
-    </aside>
-  )
-}
+//       <h2 style={{ marginTop: 48, marginLeft: 12 }}>{'Experimental'}</h2>
+//       <Link to={'/experiment/view4'}>
+//         <SidebarRow>{'View4'}</SidebarRow>
+//       </Link>
+//       <Link to={'/experiment/view5'}>
+//         <SidebarRow>{'View5'}</SidebarRow>
+//       </Link>
+//       <Link to={'/experiment/view6'}>
+//         <SidebarRow>{'View6 - Cloud Detections'}</SidebarRow>
+//       </Link>
+//       <Link to={'/experiment/view7'}>
+//         <SidebarRow>{'View7 - Terrestrial Detections'}</SidebarRow>
+//       </Link>
+//     </aside>
+//   )
+// }
