0990940dc21c7bb60868d3ba86712abf943bf77b
Alan Koshy <alankoshy16@gmail.com>
2025-08-06 18:00:04 +0530

    Remove headings from dashboard views

--- skynet-react/src/dashboards/EverythingDash.tsx
+++ skynet-react/src/dashboards/EverythingDash.tsx
@@ -7,7 +7,6 @@ export function EverythingDash()
   const config = new Config();
   return (
     <div className="min-h-screen mx-10 text-3xl font-bold pt-4 bg-black text-white mt-20">
-      <h1>Everything Dash</h1>
       <iframe className="mt-4 w-full h-[88vh] rounded-md" src={`${config.ServerURL()}:3000/view1`} />
     </div>
   )
--- skynet-react/src/dashboards/View4.tsx
+++ skynet-react/src/dashboards/View4.tsx
@@ -5,7 +5,6 @@ export function View4()
   const config = new Config();
   return (
     <div className="min-h-screen mx-10 text-3xl font-bold pt-4 bg-black text-white">
-      {/* <h1>Primary Dash</h1> */}
       <iframe className="mt-4 w-full h-[88vh] rounded-md" src={`${config.ServerURL()}:3000/experiment/view4`} />
     </div>
   )
