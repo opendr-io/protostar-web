5ba4089e7b0d88a11695eaeaa2104ca35e06efe6
anidev95 <130506893+anidev95@users.noreply.github.com>
2025-09-28 14:52:36 -0400

    Continuing work on cases interface.

--- skynet-react/src/pages/CaseDetails.tsx
+++ skynet-react/src/pages/CaseDetails.tsx
@@ -87,48 +87,49 @@ export function CaseDetails({ selected, setSelected, appService, telemetryServic
     <main className="mx-5">
       <button onClick={handleGoBack} className="bg-black text-white border border-gray-300 mt-4 w-24 h-10 py-2 rounded-md hover:bg-gray-600 font-normal cursor-pointer">Back</button>
       <div className="flex">
-        <div className="mx-2 mt-4 flex">
+        <div className="mx-4 mt-4 flex flex-col">
           <h1 className="text-xl font-semibold">Case Details</h1>
             {selected && (
-              <div className={`flex flex-col mx-4 mt-2`}>
+              <div className={`mx-4 mt-2`}>
                 <div>
                   <h1 className="text-lg font-semibold">Case Name: { selected.casename }</h1>
                   <h1 className="text-lg font-semibold">Investigate Entity: { selected.investigated_entity }</h1>
-                  <div className="mx-2 mt-2 flex-row">
+                  <div className="mx-2 mt-2">
                     <p className="text-base">Priority: { selected.priority }</p>
                     <p className="text-base">Description: { selected.description }</p>
                   </div>
                 </div>
               </div>
             )}
+          <div className="flex-row mt-10">
+            <h1>Comments</h1>
+            <div className="bg-[#1B1B1B] mt-2">
+              <p>Comment Post</p>
+              <div>
+                {comments.map((item, index) => (
+                  <li key={index} className="text-white cursor-pointer px-4 py-2 hover:bg-gray-600 list-none">
+                    <span className="mx-1">{item.f1}</span>
+                    <span className="mx-1">{item.f2}</span>
+                    <span className="mx-1 text-xs">{item.f3}</span>
+                  </li>
+                ))}
+              </div>
+            </div>
+            <form onSubmit={SubmitComment}>
+              <button type="submit" className="px-4 mt-4 cursor-pointer border mb-2 w-full py-2 bg-black text-white rounded hover:bg-gray-600 active:bg-gray-800">Post Comment</button>
+              <textarea className="block w-full p-3 border border-gray-300 rounded-md resize-none focus:ring-black transition-colors duration-200" onChange={(e) => setComment(e.target.value)} rows={6} placeholder="Enter Comment" />
+            </form>
+          </div>
         </div>
-        <div className="w-fit mr-10 mt-10">
+        <div className="max-w-4xl mx-auto mt-10 space-y-4 p-4 rounded-lg shadow-sm items-center gap-4 mb-2">
           {entityData.map((row: any, i: any) => (
             <div key={i} className="hover:bg-gray-600 cursor-pointer">
-              <div className="flex flex-row border border-gray-200 px-4 py-2 font-mono">
-                <span className="mr-4">{row.key}</span> <span>{typeof row.value === "object" ? JSON.stringify(row.value) : String(row.value)}</span>
+              <div className="flex flex-row border border-gray-200 px-4 py-2 font-mono items-center overflow-x-auto min-w-0">
+                <span className="mr-4 flex-shrink-0 whitespace-nowrap">{row.key}</span>
+                <span className="flex-shrink-0 whitespace-nowrap">{typeof row.value === "object" ? JSON.stringify(row.value) : String(row.value)}</span>
               </div>
             </div>
           ))}
-        </div>
-        <div className="flex-row mt-10">
-          <h1>Comments</h1>
-          <div className="bg-[#1B1B1B] mt-2">
-            <p>Comment Post</p>
-            <div>
-              {comments.map((item, index) => (
-                <li key={index} className="text-white cursor-pointer px-4 py-2 hover:bg-gray-600 list-none">
-                  <span className="mx-1">{item.f1}</span>
-                  <span className="mx-1">{item.f2}</span>
-                  <span className="mx-1 text-xs">{item.f3}</span>
-                </li>
-              ))}
-            </div>
-          </div>
-          <form onSubmit={SubmitComment}>
-            <button type="submit" className="px-4 mt-4 cursor-pointer border mb-2 w-full py-2 bg-black text-white rounded hover:bg-gray-600 active:bg-gray-800">Post Comment</button>
-            <textarea className="block w-full p-3 border border-gray-300 rounded-md resize-none focus:ring-black transition-colors duration-200" onChange={(e) => setComment(e.target.value)} rows={6} placeholder="Enter Comment" />
-          </form>
         </div>
       </div>
     </main>
