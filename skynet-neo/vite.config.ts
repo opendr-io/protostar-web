f1080d7731643c687637785588e892e1aaa87cf3
Anirudh <>
2025-11-11 13:11:29 -0500

    Removing info from env files.

--- skynet-neo/vite.config.ts
+++ skynet-neo/vite.config.ts
@@ -9,6 +9,6 @@ export default defineConfig({
     // @ts-ignore
     https: false,
     port: 3000,
-    allowedHosts: ['desktop-77ao329'], // <== Must be an array
+    allowedHosts: ['hostname'], // <== Must be an array
   },
 })
