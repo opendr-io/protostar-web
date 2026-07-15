ec656e63faf29ce9c95abf926dbd6cbd0f6476b3
agent-thmpson <info@opendr.io>
2026-07-15 15:02:53 -0400

    Update View6.tsx

--- protostar-neo/src/pages/Dashboard/Experimental/View6.tsx
+++ protostar-neo/src/pages/Dashboard/Experimental/View6.tsx
@@ -44,7 +44,7 @@ export function View6()
         links={network.links ?? []}
         width={"100%"}
         height={"90vh"}
-        strength={-50}
+        strength={-1400}
         labelNodeTypes={['ENTITY', 'HOST']}
         directed
       />
